a641f502b4734fd6455bedf724b38056b51ebd5d
User-2021SVQTSS\Administrator <16fekim@gmail.com>
2022-04-13 12:01:24 +0900

    Initial commit

---
+++ src/test/java/com/fekim/tagdiary/TagDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fekim.tagdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
