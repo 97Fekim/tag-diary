dc0e561dbdc4b4f7c78173a34f6dc3dd8ed6c2c3
User-2021SVQTSS\Administrator <16fekim@gmail.com>
2022-04-14 14:22:02 +0900

    Feat DiaryDTO to Diary entity method

--- src/main/java/com/fekim/tagdiary/service/DiaryService.java
+++ src/main/java/com/fekim/tagdiary/service/DiaryService.java
@@ -1,6 +1,66 @@
 package com.fekim.tagdiary.service;
 
+import com.fekim.tagdiary.dto.DiaryDTO;
+import com.fekim.tagdiary.dto.WriteUpDTO;
+import com.fekim.tagdiary.entity.Diary;
+import com.fekim.tagdiary.entity.Member;
+import com.fekim.tagdiary.entity.Tag;
+import com.fekim.tagdiary.entity.WriteUp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public interface DiaryService {
 
+    // 일기 등록
+    Long register(DiaryDTO diaryDTO);
+
+    /* DiaryDTO -> Diary
+     * DiaryDTO가 WriteUp을 List로 참조하고 있기때문에
+     * 반환타입을 Map<String, Object>으로 지정 */
+    default Map<String, Object> dtoToEntity(DiaryDTO diaryDTO){
+
+        Map<String, Object> entityMap = new HashMap<>();
+
+        /* Diary 처리 (엔티티 Diary생성) */
+        Diary diary = Diary.builder()
+                .dno(diaryDTO.getDno())
+                .title(diaryDTO.getTitle())
+                .writer(Member.builder()
+                        .id(diaryDTO
+                        .getWriter())
+                        .build())
+                .build();
+
+        /* List<WriteUp> 처리 */
+        List<WriteUpDTO> writeUpDTOList = diaryDTO.getWriteUpDTOList();
+
+        if(writeUpDTOList != null && writeUpDTOList.size() > 0){    // List가 존재하면(일기에 태그와 내용이 있으면)
+
+            /* 엔티티 WriteUp 리스트 생성 */
+            List<WriteUp> writeUpList = writeUpDTOList.stream().map(writeUpDTO -> {
+
+                WriteUp writeUp = WriteUp.builder()
+                        .wno(writeUpDTO.getWno())
+                        .content(writeUpDTO.getContent())
+                        .tag(Tag.builder()
+                                .tno(writeUpDTO.getTagDTO().getTno())
+                                .tagName(writeUpDTO.getTagDTO().getName())
+                                .tagType(writeUpDTO.getTagDTO().getType())
+                                .build())
+                        .build();
+
+                return writeUp;
+
+            }).collect(Collectors.toList());
+
+            entityMap.put("writeUpList", writeUpList);
+        }
+
+        return entityMap;
+    }
 
 }
--- src/main/java/com/fekim/tagdiary/service/DiaryServiceImpl.java
+++ src/main/java/com/fekim/tagdiary/service/DiaryServiceImpl.java
@@ -1,5 +1,9 @@
 package com.fekim.tagdiary.service;
 
+import com.fekim.tagdiary.dto.DiaryDTO;
+import com.fekim.tagdiary.repository.DiaryRepository;
+import com.fekim.tagdiary.repository.TagRepository;
+import com.fekim.tagdiary.repository.WriteUpRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -10,5 +14,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DiaryServiceImpl implements DiaryService{
 
+    private final DiaryRepository diaryRepository;
 
+    private final WriteUpRepository writeUpRepository;
+
+    private final TagRepository tagRepository;
+
+    @Override
+    public Long register(DiaryDTO diaryDTO) {
+        return null;
+    }
 }
