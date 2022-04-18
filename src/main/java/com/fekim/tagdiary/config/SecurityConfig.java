3bb6ba2de0776fcda6d0ba4a473f830db496d213
97Fekim <rlatmdcjf9@gmail.com>
2022-04-18 16:51:34 +0900

    FEAT: Create OAuth login

--- src/main/java/com/fekim/tagdiary/config/SecurityConfig.java
+++ src/main/java/com/fekim/tagdiary/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -28,7 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin();   // 인가/인증에 문제시 로그인 화면, .loginPage()나 .loginProcessUrl() 을 이용해서 커스텀 로그인 페이지 적용 가능
         http.csrf().disable();
-        http.logout();      // .logoutPage()나 .logoutProcessUrl()을 이용해서 커스텀 로그아웃 페이지 적용 가능
+        // http.logout();      // .logoutPage()나 .logoutProcessUrl()을 이용해서 커스텀 로그아웃 페이지 적용 가능
+        http.oauth2Login();
 
     }
 
--- src/main/java/com/fekim/tagdiary/dto/AuthMemberDTO.java
+++ src/main/java/com/fekim/tagdiary/dto/AuthMemberDTO.java
@@ -6,14 +6,16 @@ import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 
 @Log4j2
 @Setter
 @Getter
 @ToString
-public class AuthMemberDTO extends User {
+public class AuthMemberDTO extends User implements OAuth2User {
 
     private Long id;
     private String name;
@@ -21,6 +23,10 @@ public class AuthMemberDTO extends User {
     private String password;
     private boolean fromSocial;
 
+    private Map<String, Object> attr;
+
+
+    /* 일반 로그인 */
     public AuthMemberDTO(String username,
                          String password,
                          boolean fromSocial,
@@ -33,4 +39,22 @@ public class AuthMemberDTO extends User {
         // id와 name은 Service에서 set
     }
 
+    /* OAuth 로그인 */
+    public AuthMemberDTO(
+            String username,
+            String password,
+            boolean fromSocial,
+            Collection<? extends GrantedAuthority> authorities,
+            Map<String, Object> attr) {
+
+        this(username, password, fromSocial, authorities);
+        this.attr = attr;
+        // id와 name은 Service에서 set
+
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attr;
+    }
 }
