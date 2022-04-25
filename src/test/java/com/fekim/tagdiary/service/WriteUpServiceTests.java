486f90a5562f0f1fd4f62847e1675e071f8f21f8
97Fekim <rlatmdcjf9@gmail.com>
2022-04-25 16:44:58 +0900

    TEST: test code commented for deploy test

--- src/test/java/com/fekim/tagdiary/service/WriteUpServiceTests.java
+++ src/test/java/com/fekim/tagdiary/service/WriteUpServiceTests.java
@@ -1,14 +1,14 @@
-package com.fekim.tagdiary.service;
-
-import com.fekim.tagdiary.writeup.service.WriteUpService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class WriteUpServiceTests {
-
-    @Autowired
-    private WriteUpService writeUpService;
-
-
-}
+//package com.fekim.tagdiary.service;
+//
+//import com.fekim.tagdiary.writeup.service.WriteUpService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class WriteUpServiceTests {
+//
+//    @Autowired
+//    private WriteUpService writeUpService;
+//
+//
+//}
